e3279ec771812e0ae0ce5e32fa10bd2e2dbaa478
Francois Perrad <francois.perrad@gadz.org>
2007-10-12 07:21:15 +0000

    Import of PERRAD/CORBA-XPIDL-0.20 from CPAN.
    gitpan-cpan-distribution: CORBA-XPIDL
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         PERRAD/CORBA-XPIDL-0.20.tar.gz
    gitpan-cpan-author:       PERRAD
    gitpan-cpan-maturity:     released

--- lib/CORBA/XPIDL.pm
+++ lib/CORBA/XPIDL.pm
@@ -1,9 +1,39 @@
-use strict;
 
 package CORBA::XPIDL;
 
-use vars qw($VERSION);
-$VERSION = '0.04';
+use strict;
+use warnings;
+
+our $VERSION = '0.20';
+
+=head1 DESCRIPTION
+
+This module is used by the following scripts :
+
+=over 4
+
+=item L<xpidl>
+
+=item L<xpt_dump>
+
+=item L<xpt_link>
+
+=back
+
+=head1 AUTHORS
+
+The original version (C language) is mozilla.org code.
+
+Port to Perl by Francois Perrad, francois.perrad@gadz.org
+
+=head1 COPYRIGHT
+
+Copyright 2004-2007, Francois Perrad.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
 
